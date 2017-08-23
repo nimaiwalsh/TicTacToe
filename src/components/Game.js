fd9d18117e65b4a35dc093bad66a95164cb00f27
nimaiwalsh <nimai.walsh@gmail.com>
2017-08-23 19:51:52 +1000

    worked on Computer moves functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,29 +12,77 @@ class Game extends React.Component {
         }
       ],
       xIsNext: true,
+      stepNumber: 0,
+      onePlayer: true,
+      computerMove: false,
+      playerToken: 'X'
     };
   }
 
   //Using history, work with current squares and update history with new array
-  //Use.slice() to copy the squares array instead of mutating the existing array.
   //If the game is won or square is already filled, return
   //Determine weather X or O is next
-  handleClick(i) {
-    const history = this.state.history;
+  //Update state and add new moves as a new object in the history array 
+  handleClick(i, computerMove) {
+    const history = this.state.history.slice(0, this.state.stepNumber +1);
     const current = history[history.length -1];
     const squares = current.squares.slice();
     if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+  
     this.setState({
-      history: history.concat({
-        squares: squares
-      }),
-      xIsNext: !this.state.xIsNext
+      history: history.concat(
+        {
+          squares: squares
+        }
+      ),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+      computerMove: computerMove,
     });
   }
 
+  computerMove() {
+    const history = this.state.history;
+    const current = history[history.length - 1].squares;
+    const playerToken = this.state.playerToken;
+    
+    const drawSquare = (drawSquare) => {
+      setTimeout(() => {this.handleClick(drawSquare, false)}, 2000);
+    }
+
+    //If player clicks centre go in corner square
+    if (current[4] === playerToken) {
+      const posMoves = [0, 2, 6, 8];
+      let move = posMoves[Math.floor(Math.random() * posMoves.length)];
+      drawSquare(move);
+    } 
+    //if player click corner square go in centre square
+    else if (
+      current[0] === playerToken || 
+      current[2] === playerToken ||
+      current[6] === playerToken ||
+      current[8] === playerToken
+    ) {  
+      drawSquare(4);
+    }
+
+    //if player has 2 in a row, place in 3rd square
+   
+  }
+
+  //Jump to an older move in the history by updating step number
+  jumpTo(move) {
+    this.setState({
+      stepNumber: move,
+      //X is next on all even numbers
+      xIsNext: (move % 2) === 0,
+    });
+  }
+
+  //Determine the winning sequence
   calculateWinner(squares) {
     const winningLines = [
       [0, 1, 2],
@@ -53,13 +101,19 @@ class Game extends React.Component {
         return squares[a];
       }
     }
-    
     return null;
   }
 
   render() {
+    //Determine One player mode and start computer turn
+    if(this.state.onePlayer && this.state.computerMove) {
+      this.computerMove()
+    };
+    //Assign current move from history to display correct squares
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
+
+    //After each move, see if there is a winner else alternate player
     const winner = this.calculateWinner(current.squares);
     let status;
     if (winner) {
@@ -68,17 +122,51 @@ class Game extends React.Component {
       status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
     }
 
+    //Show the history of each move and jumpTo a previous move on click
+    const moves = this.state.history.map((step, move) => {
+      const moveDesc = move ? `Move Number: ${move}` : 'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{moveDesc}</a>
+        </li>
+      );
+    });
+
+    //Show players
+    let players = null;
+    (this.state.onePlayer) ? players = 'One Player' : players = 'Two Players';
+
     return (
       <div className="game">
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={(i) => this.handleClick(i, true)}
           />
         </div>
+        <div className="game-options">
+          <div className="game-options-player">
+            <p>Game type</p>
+            <button onClick={() => {this.setState({onePlayer: true})}}>One Player</button>
+            <button onClick={() => {this.setState({onePlayer: false})}}>Two Player</button>
+            <p>Would you like to be</p>
+          </div>
+          <div className="game-options-icon">
+            <button onClick={() => {this.setState({
+              xIsNext: true, 
+              playerToken: 'X'
+            })}}>X</button>
+            or
+            <button onClick={() => {this.setState({
+              xIsNext: false,
+              playerToken: 'O'
+            })}}>0</button>
+          </div>
+        </div>
         <div className="game-info">
+          <div>{players}</div>
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
